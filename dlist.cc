904b245c638f4cc6a26909c0b0cd00a2306e70df
Yang Su <suyang1+services@gmail.com>
2011-10-10 21:56:57 -0400

    Revised dlist implementation

--- dlist.cc
+++ dlist.cc
@@ -1,40 +1,66 @@
 template <typename T>
-bool Dlist<T>::isEmpty() {
-  return first == NULL && last == NULL;
+Dlist<T>::Node::Node(T* o) {
+  this->o = o;
+}
+
+template <typename T>
+Dlist<T>::Dlist(const Dlist &l) {
+  this->makeEmpty();
+  this->copyAll(l);
+}
+
+template <typename T>
+Dlist<T>& Dlist<T>::operator= (const Dlist &l) {
+  removeAll();
+  copyAll(l);
+  return *this;
+}
+
+template <typename T>
+void Dlist<T>::removeAll() {
+  while(!this->isEmpty())
+    this->removeFront();
+}
+
+template <typename T>
+void Dlist<T>::copyAll(const Dlist &l) {
+  Node* copyNode = l.first;
+  while(copyNode != NULL) {
+    this->insertBack(new T(*(copyNode->o)));
+    copyNode = copyNode->next;
+  }
 }
 
 template <typename T>
 void Dlist<T>::insertFront(T* o) {
-  node* temp = new node;
-  temp->o = o;
+  Node* newNode = new Node(o);
   if (this->isEmpty()) {
-    first = temp;
+    first = newNode;
     first->prev = NULL;
     first->next = NULL;
     last = first;
   }
   else {
-    first->prev = temp;
-    temp->next = first;
-    temp->prev = NULL;
+    first->prev = newNode;
+    newNode->next = first;
+    newNode->prev = NULL;
     first = first->prev;
   }
 }
 
 template <typename T>
 void Dlist<T>::insertBack(T* o) {
-  node* temp = new node;
-  temp->o = o;
+  Node* newNode = new Node(o);
   if (this->isEmpty()) {
-    last = temp;
+    last = newNode;
     last->prev = NULL;
     last->next = NULL;
     first = last;
   }
   else {
-    last->next = temp;
-    temp->prev = last;
-    temp->next = NULL;
+    last->next = newNode;
+    newNode->prev = last;
+    newNode->next = NULL;
     last = last->next;
   }
 }
@@ -44,15 +70,15 @@ T* Dlist<T>::removeFront() {
   if (this->isEmpty())
     throw emptyList();
   else {
-    node* temp = first;
-    if (first->next == NULL) //only one element in the list
+    Node* front = first;
+    if (first->next)
       this->makeEmpty();
     else {
       first = first->next;
       first->prev = NULL;
     }
-    T* o = temp->o;
-    delete temp;
+    T* o = front->o;
+    delete front;
     return o;
   }
 }
@@ -62,67 +88,15 @@ T* Dlist<T>::removeBack() {
   if (this->isEmpty())
     throw emptyList();
   else {
-    node* temp = last;
-    if (last->prev == NULL) //only one element in the list
+    Node* back = last;
+    if (last->prev)
       this->makeEmpty();
     else {
       last = last->prev;
       last->next = NULL;
     }
-    T* o = temp->o;
-    delete temp;
+    T* o = back->o;
+    delete back;
     return o;
   }
-}
-
-template <typename T>
-Dlist<T>::Dlist() { this->makeEmpty(); }
-
-template <typename T>
-Dlist<T>::Dlist(const Dlist &l) {
-  this->makeEmpty();
-  copyAll(l);
-}
-
-template <typename T>
-Dlist<T>& Dlist<T>::operator = (const Dlist &l) {
-  removeAll();
-  copyAll(l);
-  return *this;
-}
-template <typename T>
-Dlist<T>::~Dlist() {
-  removeAll();
-}
-
-template <typename T>
-void Dlist<T>::makeEmpty() {
-  first = NULL;
-  last = NULL;
-}
-
-template <typename T>
-void Dlist<T>::removeAll() {
-  node* current;
-  while(first != NULL) {
-    current = first;
-    first = first->next;
-    delete current->o;
-    delete current;
-  }
-  current = NULL;
-  first = NULL;
-  last = NULL;
-}
-
-template <typename T>
-void Dlist<T>::copyAll(const Dlist &l) {
-  node* copyNode = l.first;
-  T* obj;
-  while(copyNode != NULL) {
-    obj = new T(*(copyNode->o));
-    insertBack(obj);
-    copyNode = copyNode->next;
-  }
-  obj = NULL;
 }
--- dlist.h
+++ dlist.h
@@ -1,69 +1,41 @@
 #ifndef __DLIST_H__
 #define __DLIST_H__
 
-class emptyList {
-    // OVERVIEW: an exception class
-};
+class emptyList {};
 
 template <typename T>
 class Dlist {
-    // OVERVIEW: contains a double-ended list of Objects
+public:
+  Dlist() { this->makeEmpty(); }
+  Dlist(const Dlist &l);
+  Dlist& operator=(const Dlist& l);
+  ~Dlist() { removeAll(); }
 
- public:
+  bool isEmpty() { return first == NULL && last == NULL; }
+  
+  void insertFront(T* o);
+  void insertBack(T* o);
+  T* peekFront();
+  T* peekBack();
+  T* removeFront();
+  T* removeBack();
+  
+  void clear() { this->removeAll(); }
 
-    // Operational methods
+private:
+  struct Node {
+    Node* next;
+    Node* prev;
+    T* o;
+    Node(T*);
+  };
 
-    bool isEmpty();
-    // EFFECTS: returns true if list is empy, false otherwise
+  Node* first;
+  Node* last;
 
-    void insertFront(T *o);
-    // MODIFIES this
-    // EFFECTS inserts o at the front of the list
-    
-    void insertBack(T *o);
-    // MODIFIES this
-    // EFFECTS inserts o at the back of the list
-
-    T *removeFront();
-    // MODIFIES this
-    // EFFECTS removes and returns first object from non-empty list
-    //         throws an instance of emptyList if empty
-
-    T *removeBack();
-    // MODIFIES this
-    // EFFECTS removes and returns last object from non-empty list
-    //         throws an instance of emptyList if empty
-
-    // Maintenance methods
-    Dlist();                                   // ctor
-    Dlist(const Dlist &l);                     // copy ctor
-    Dlist &operator=(const Dlist &l);          // assignment
-    ~Dlist();                                  // dtor
-
- private:
-    // A private type
-    struct node {
-	node   *next;
-	node   *prev;
-	T      *o;
-    };
-
-    node   *first; // The pointer to the 1st node (NULL if none)
-    node   *last;  // The pointer to the 2nd node (NULL if none)
-
-    // Utility methods
-
-    void makeEmpty();
-    // EFFECT: called by constructors/operator= to establish empty
-    // list invariant
-    
-    void removeAll();
-    // EFFECT: called by destructor/operator= to remove and destroy
-    // all list elements
-
-    void copyAll(const Dlist &l);
-    // EFFECT: called by copy constructor/operator= to copy elements
-    // from a source instance l to this instance
+  void makeEmpty() { first = last = NULL; }
+  void removeAll();
+  void copyAll(const Dlist &l);
 };
 
 /*
