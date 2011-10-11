9c4d7fba9419c1b65b253a5b2c8941118c9cbb0d
Yang Su <suyang1+services@gmail.com>
2011-10-11 00:35:16 -0400

    calculator rewrite

--- calc.cc
+++ calc.cc
@@ -2,213 +2,202 @@
 #include <string>
 #include <cstdlib>
 #include "dlist.h"
-using namespace std;
 
-void notEnoughOperandError() { cout << "Not enough operands" << endl; }
-void badInputError() { cout << "Bad input" << endl; }
+using namespace std;
+void notEnoughOperandsError(const char* op) { 
+  cout << op << ": Not enough operands" << endl; 
+}
+void badInputError(string input) { cout << "Bad input: " << input << endl; }
 void divZeroError() { cout << "Divide by zero" << endl; }
 
-void add(Dlist<double>* stack){
+Dlist<double> stack;
+void add() {
   double* operand1 = NULL, *operand2 = NULL;
   try{
-    operand1 = stack->removeFront();
-    operand2 = stack->removeFront();
-    double* result = new double(*operand2+*operand1);
-    stack->insertFront(result);
-    result = NULL;
+    operand1 = stack.removeFront();
+    operand2 = stack.removeFront();
+    stack.insertFront(new double(*operand2 + *operand1));
     delete operand1;
     delete operand2;
   }
-  catch(emptyList e){
-    if(operand1) stack->insertFront(operand1);
-    notEnoughOperandError();
-  }
-}
-void sub(Dlist<double>* stack){
-  double* operand1 = NULL,*operand2 = NULL;
-  try{
-    operand1 = stack->removeFront();
-    operand2 = stack->removeFront();
-    double* result = new double(*operand2-*operand1);
-    stack->insertFront(result);
-    result = NULL;
-    delete operand1;
-    delete operand2;
-  }
-  catch(emptyList e){
-    if(operand1) stack->insertFront(operand1);
-    notEnoughOperandError();
-  }
-}
-void mult(Dlist<double>* stack){
-  double* operand1 = NULL,*operand2 = NULL;
-  try{
-    operand1 = stack->removeFront();
-    operand2 = stack->removeFront();
-    double* result = new double(*operand2 * *operand1);
-    stack->insertFront(result);
-    result = NULL;
-    delete operand1;
-    delete operand2;
-  }
-  catch(emptyList e){
-    if(operand1) stack->insertFront(operand1);
-    notEnoughOperandError();
+  catch(emptyList e) {
+    if (operand1) stack.insertFront(operand1);
+    notEnoughOperandsError("add");
   }
 }
 
-void div(Dlist<double>* stack){
+void sub() {
   double* operand1 = NULL,*operand2 = NULL;
   try{
-    operand1 = stack->removeFront();
-    operand2 = stack->removeFront();
-    if(*operand1 == 0){
+    operand1 = stack.removeFront();
+    operand2 = stack.removeFront();
+    stack.insertFront(new double(*operand2 - *operand1));
+    delete operand1;
+    delete operand2;
+  }
+  catch(emptyList e) {
+    if (operand1) stack.insertFront(operand1);
+    notEnoughOperandsError("sub");
+  }
+}
+
+void mult() {
+  double* operand1 = NULL,*operand2 = NULL;
+  try{
+    operand1 = stack.removeFront();
+    operand2 = stack.removeFront();
+    stack.insertFront(new double(*operand2 * *operand1));
+    delete operand1;
+    delete operand2;
+  }
+  catch(emptyList e) {
+    if (operand1) stack.insertFront(operand1);
+    notEnoughOperandsError("mult");
+  }
+}
+
+void div() {
+  double* operand1 = NULL,*operand2 = NULL;
+  try{
+    operand1 = stack.removeFront();
+    operand2 = stack.removeFront();
+    if (*operand1 == 0) {
       divZeroError();
-      stack->insertFront(operand2);
-      stack->insertFront(operand1);
+      stack.insertFront(operand2);
+      stack.insertFront(operand1);
     }
     else{
-      double* result = new double(*operand2 / *operand1);
-      stack->insertFront(result);
-      result = NULL;
+      stack.insertFront(new double(*operand2 / *operand1));
       delete operand1;
       delete operand2;
     }
   }
-  catch(emptyList e){
-    if(operand1) stack->insertFront(operand1);
-    notEnoughOperandError();
+  catch(emptyList e) {
+    if (operand1) stack.insertFront(operand1);
+    notEnoughOperandsError("div");
   }
 }
 
-void neg(Dlist<double>* stack){
-  double* operand;
-  if(stack->isEmpty()){
-    cout  <<  "Not enough operands"  <<  endl;
-  }
-  else{
-    operand = stack->removeFront();
-    *operand = -*operand;
-    stack->insertFront(operand);
-  }
-  operand = NULL;
+void neg() {
+  if (stack.isEmpty()) 
+    notEnoughOperandsError("neg");
+
+  double* operand = stack.peekFront();
+  *operand = -*operand;
 }
 
-void dup(Dlist<double>* stack){
-  double* operand;
-  if(!stack->isEmpty()){
-    operand = stack->removeFront();
-    double* copy = new double(*operand);
-    stack->insertFront(operand);
-    stack->insertFront(copy);
-  }
-  else{
-    notEnoughOperandError();
-  }
-  operand = NULL;
-}
-
-
-void print(Dlist<double>* stack){
-  double* operand;
-  if(!stack->isEmpty()){
-    operand = stack->removeFront();
-    cout << *operand << endl;
-    stack->insertFront(operand);
-  }
-  else{
-    notEnoughOperandError();
-  }
-  operand = NULL;
-}
-
-void clear(Dlist<double>* stack){
-  stack->~Dlist();
-}
-
-void rev(Dlist<double>* stack){
+void rev() {
   double* operand1 = NULL,*operand2 = NULL;
   try{
-    operand1 = stack->removeFront();
-    operand2 = stack->removeFront();
-    stack->insertFront(operand1);
-    stack->insertFront(operand2);
+    operand1 = stack.removeFront();
+    operand2 = stack.removeFront();
+    stack.insertFront(operand1);
+    stack.insertFront(operand2);
   }
-  catch(emptyList e){
-    if(operand1) stack->insertFront(operand1);
-    notEnoughOperandError();
+  catch(emptyList e) {
+    if (operand1) stack.insertFront(operand1);
+    notEnoughOperandsError("rev");
   }
 }
-void printa(Dlist<double>* stack){
+
+void dup() {
+  if (stack.isEmpty())
+    notEnoughOperandsError("dup");
+
+  stack.insertFront(new double(*stack.peekFront()));
+}
+
+
+void print() {
+  if (stack.isEmpty())
+    notEnoughOperandsError("print");
+
+  cout << *stack.peekFront() << endl;
+}
+
+void printa() {
   double* operand;
   Dlist<double> temp;
-  bool first = true;
-  while(!stack->isEmpty()){
-    operand = stack->removeFront();
-    if(first){
-      cout << *operand;
-      first = false;
-    }
-    else
-      cout << " " << *operand;
+  cout << *stack.removeFront();
+  while(!stack.isEmpty()) {
+    operand = stack.removeFront();
+    cout << " " << *operand;
     temp.insertBack(operand);
   }
   cout << endl;
-  *stack = temp;
-  operand = NULL;
+  stack = temp;
 }
 
-int main(){
-  Dlist<double>* stack = new Dlist<double>;
-  string input;
-  unsigned int dotc = 0;
-  unsigned int digitc = 0;
-  while(true){
-    getline(cin,input);
-    if(input.compare("q") == 0)
-      return 0;
-    else if(input.compare("a") == 0)
-      printa(stack);                
-    else if(input.compare("p") == 0)
-      print(stack);                 
-    else if(input.compare("d") == 0)
-      dup(stack);                   
-    else if(input.compare("n") == 0)
-      neg(stack);                   
-    else if(input.compare("c") == 0)
-      clear(stack);                 
-    else if(input.compare("r") == 0)
-      rev(stack);                   
-    else if(input.compare("+") == 0)
-      add(stack);                   
-    else if(input.compare("-") == 0)
-      sub(stack);                   
-    else if(input.compare("*") == 0)
-      mult(stack);                  
-    else if(input.compare("/") == 0)
-      div(stack);
-    else { //number
-      for(unsigned int i = 0; i < input.size(); i++){
-        if(input[i] == '.'){
-          dotc++;
-          if (!(dotc > 1 || i == 0 || i == input.size() - 1)){
-            digitc++;
-          }
-        }
-        if (input[i] >= '0'&&input[i] <= '9'){
-          digitc++;
-        }
-      }
-      if(digitc == input.size() && digitc >= 1){
-        double* num = new double(atof(input.c_str()));
-        stack->insertFront(num);
-      }
-      else{
-        badInputError();
-      }
-      digitc = 0;
-      dotc = 0;
+void clear() { stack.clear(); }
+void help();
+
+typedef void (*cmdFunc)();
+struct Command {
+  string cmd;
+  cmdFunc func;
+  string funcName;
+};
+Command cmds[] = {
+  {"*", mult, "multiply"},
+  {"+", add, "add"},
+  {"-", sub, "subtract"},
+  {"/", div, "divide"},
+  {"a", printa, "print all"},
+  {"c", clear, "clear"},
+  {"d", dup, "duplicate top item"},
+  {"n", neg, "negate top item"},
+  {"p", print, "print top item"},
+  {"r", rev, "reverse top 2 items"},
+  {"help", help, "help"}
+};
+
+void help() {
+  int end = sizeof(cmds)/sizeof(Command);
+  cout << end << " commands:" << endl;
+  for (unsigned int i = 0; i < end; ++i) {
+    cout << "  " << cmds[i].cmd << " : " << cmds[i].funcName << endl;
+  }
+}
+
+bool execCommand(string input) {
+  int end = sizeof(cmds)/sizeof(Command);
+  for (unsigned int i = 0; i < end; ++i) {
+    if (input.compare(cmds[i].cmd) == 0) {
+      cmds[i].func();
+      return true;
     }
+  }
+  return false;
+}
+
+bool addInteger(string input) {
+  int digitc = 0, dotc = 0;
+  for(unsigned int i = 0; i < input.size(); i++) {
+    if (input[i] == '.') {
+      dotc++;
+      if (!(dotc > 1 || i == 0 || i == input.size() - 1))
+        digitc++;
+    }
+    if (input[i] >= '0'&&input[i] <= '9')
+      digitc++;
+  }
+  if (digitc == input.size() && digitc >= 1) {
+    double* num = new double(atof(input.c_str()));
+    stack.insertFront(num);
+    return true;
+  }
+  return false;
+}
+int main() {
+  string input;
+  while(true) {
+    getline(cin,input);
+    if (input.compare("q") == 0)
+      break;
+    if (execCommand(input) || addInteger(input))
+      continue;
+    else
+      badInputError(input);
   }
   return 0;
 }
